0ecf512d28cc89fe1fd7c34b3d866cdda900fcb2
Aya <ayahathout1@gmail.com>
2025-02-01 03:31:43 +0200

    add tablesCreation

--- daosAndEntities/entities/User.java
+++ daosAndEntities/entities/User.java
@@ -3,21 +3,6 @@ package org.example;
 import java.sql.Blob;
 import java.time.LocalDate;
 
-// The creation of the table
-//create table Users(
-//        userId int primary key auto_increment,
-//        phone varchar(255) unique not null,
-//        displayName varchar(255) not null,
-//        userEmail varchar(255) unique null,
-//        picture blob null,
-//        password varchar(255) not null,
-//        gender enum ("Male", "Female") null,
-//        country varchar(255) null,
-//        birthday date null,
-//        bio varchar(1000) null,
-//        userStatus enum("Offline", "Available", "Busy", "Away") null
-//        );
-
 public class User {
     // Attributes
     private long userId;
