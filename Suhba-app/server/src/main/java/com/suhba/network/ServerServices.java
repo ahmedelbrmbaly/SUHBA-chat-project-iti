7cbae686eeeea8138a03df08f0af535e22b23e11
Ahmed Elbrmbaly <ahmed.elbrmbaly@gmail.com>
2025-02-04 23:16:26 +0200

    updated the services

--- Suhba-app/server/src/main/java/com/suhba/network/ServerServices.java
+++ Suhba-app/server/src/main/java/com/suhba/network/ServerServices.java
@@ -9,63 +9,91 @@ import com.suhba.database.entities.User;
 
 public interface ServerServices {
     // 1- Signup (Take User Info and return the user id)
-    public int signup(User user);
+    //DTOS
+    // Upload the photo picture  >> how
+
+    public User signup(User user);
     
     // 2- Log in
+    // >> Remeber me XML SESSION >> To BE REVIEWED
+    public boolean isPhoneRegistered(String phoneNumder);
+
     public User login(String phoneNumder , String password) ;
 
-    // 3- Logout
-    public void logout();
+    // forget password >> To BE REVIEWED (bouns)
+
+    // 3- Logout >> To BE REVIEWED
+    public boolean logout();
+
+    // Exit >> To BE REVIEWED
+    public boolean exit();
 
 /////////////////////////////////////////////////////////////////////////////////
 
-    // 4- Load user chats
-    public List<Chat> getUserChats(long userId) ;
+// CHAT SCREEN METHODS
 
+    // 4- Load user chats
+//    public List<Chat> getUserChats(long userId) ;
+    public long createPrivateChat(Long userId);
+
+    public  Map <User, Message> getUserChats(long userId) ;
     // 5- Load user Groups
-    public List<Group> getUserGroups(long userId);
+
 
     // 6- Open user-user Chat and load Messages
-    public List<Message> getChatMessages(long chatId);
+    // Message Oject needs sender name >>>
+    public List<Message> getMessages(long chatId);
+    public  User getUserById(long userId);
+    public User getPrivateUserPartnerByChat(long chatId);
 
-    // 7- Open user-group Chat and load Messages
-    public List<Message> getGroupChatMessages(long groupId);
+    // 8- Send Message user-user
+    // File trasnfer handling >> To Be Searched
+    public Message sendMessage (Message msg);
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+    // GRUUP SCREEEN
+
+    public Map <Group, Message> getUserGroups(long userId);
+    public List<User> getGroupMembers(long groupId);
+//    public List<Message> getMessages(long chatId); >> Method defined in chat above
+    //public Message sendMessage (Message msg); >> Method defined in chat above
+    public Group createGroupChat(Group group, List<Long> userID);
+    public Group getGroupByChat(long chatId);
+    public boolean addUsersToGroup(long chatId, List<Long> userID);
+    public boolean removeUsersFromGroup(long chatId, List<Long> userID);
 
 //////////////////////////////////////////////////////////////////////////////
 
-    // 8- Send Message user-user 
-    public long sendChatMessage (Message msg);
 
-    // 9- Send Message user-group
-    public long sendGroupMessage(Message msg) ;
 
-//////////////////////////////////////////////////////////////////////////////
 
     // 10- Create chat with user
-    public long createPrivateChat(Chat chat);//?
+    //?
 
     // 11- Create chat with group
-    public long createGroupChat(Chat chat, List<User> users);
+
 
     // 12- Add users to group
-    public void addUsersToGroup(long chatId, List<User> users);
+
 
 //////////////////////////////////////////////////////////////////////////////
 
-    // 13- get chat by id
-    public Chat getChat(long chatId);
+
 
 
     // 14- get another user by chat id
-    public User getPrivateUserPartnerByChat(long chatId);
+
 
 //////////////////////////////////////////////////////////////////////////////
 
     // 15- get group by chat id
-    public Group getGroupByChat(long chatId);
+
 
     // 16- get group members by group id (Or by chat id?)
-    public List<User> getGroupMembers(long groupId);
+
 
     // 17- get 
 
